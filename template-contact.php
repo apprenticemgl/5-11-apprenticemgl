748fcc5bf2360501d81f43058ff3510ec7643c52
apprenticemgl <82880233+apprenticemgl@users.noreply.github.com>
2021-05-11 20:55:32 +0800

    Create template-contact.php

---
+++ template-contact.php
@@ -0,0 +1,15 @@
+<?php
+/* Template name: Xolboo Barix */
+?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			the_content();
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
