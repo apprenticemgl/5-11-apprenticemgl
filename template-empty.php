f580d51d69e6b199e6ee1e3a0ce589535530200f
apprenticemgl <82880233+apprenticemgl@users.noreply.github.com>
2021-05-11 20:45:39 +0800

    Create template-empty.php

---
+++ template-empty.php
@@ -0,0 +1,17 @@
+<?php
+/* Template name: About us */
+?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php the_content(); ?>
+
+            <?php
+            
+            ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
